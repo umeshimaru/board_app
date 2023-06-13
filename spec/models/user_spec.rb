6381bb3e3275870867ce05891eefd66b04227e35
umekishimaru <riki12344649@gmail.com>
2023-06-13 17:29:37 +0900

    rspec_user_model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+
+it'#名、メール、パスワードがあれば有効な状態であること' do 
+user = User.new(
+                name: "ルフィ",
+                email: "rufy@yahoo.co.jp",
+                password: "gomugomu", 
+)
+expect(user).to be_valid
+end 
 end
