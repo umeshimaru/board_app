994bbb6dfbb0ddec10b17f058f9df8f17007c79a
umekishimaru <riki12344649@gmail.com>
2023-06-21 18:25:50 +0900

    フレンドリーフォワーディング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,12 +48,14 @@ class UsersController < ApplicationController
 
   def logged_in_user 
     unless logged_in?
+      store_location
       flash[:danger] = "ログインしてください"
       redirect_to login_url
     end
   end
+
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current_user
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
