b10f0c47d09896eb7059fc5902d8be03f8bed563
umekishimaru <riki12344649@gmail.com>
2023-06-22 23:44:04 +0900

    token

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@ class User < ApplicationRecord
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :  BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
   end
+
+   # ランダムなトークンを返す
+   def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
