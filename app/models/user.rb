8cc5e0e58e130f9b6c0ab58761aa74a43cc5e6c8
umekishimaru <riki12344649@gmail.com>
2023-06-28 17:23:46 +0900

    アカウント有効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,15 @@ class User < ApplicationRecord
   end
 
 
+ def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+   # 有効化用のメールを送信する
+   def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+
 end
