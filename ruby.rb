7baa952de9039dc54de6f9d81e029b22b78e2b07
umekishimaru <riki12344649@gmail.com>
2023-06-28 17:41:17 +0900

    有効化終了

--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,16 @@ else
 end
 
 
-論理否定
+①論理否定
+②selfの省略
+
+
+attr_accessor :remember_token, :activation_token
+
+def create_activation_digest
+  self.activation_token  = User.new_token
+  self.activation_digest = User.digest(activation_token)
+end
+↑なんでselfがいるか
+
+今回のselfが省略できない理由は、インスタンス変数への代入とローカル変数の初期化を区別するためであり、selfを省略するとローカル変数として扱われ、期待したインスタンス変数への代入が行われなくなるためです。
